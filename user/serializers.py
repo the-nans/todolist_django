904a0fc490b57f2a6fa37f3608f800cf732daf8f
the-Nans <agata.a.bobyleva@gmail.com>
2021-11-04 01:16:07 +0300

    just to change smth

--- user/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import User
-
-
-class UserModelSerializer(HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
-        # fields = '__all__'
--- user/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import User
-from .serializers import UserModelSerializer
-
-
-class UserModelViewSet(ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserModelSerializer
